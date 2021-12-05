0d4ee2bf31d8b14af5fd6c2de344834c173b82d0
kobori <46224993+TomoroKobori@users.noreply.github.com>
2021-12-05 11:59:16 +0900

    feat: オーナー名、リポジトリ名、見積もりラベルのカラーコードをを環境変数から取得

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use clap::{App, Arg};
 use serde::Deserialize;
 use reqwest::{Error};
+use std::env;
 
 #[derive(Deserialize, Debug)]
 struct Issue {
@@ -20,6 +21,22 @@ struct Label {
 #[derive(Deserialize, Debug)]
 struct Milestone {
     title: String
+}
+
+struct Config {
+    github_owner: String,
+    github_repo: String,
+    github_estimate_color_code: String
+}
+
+impl Config {
+    pub fn new() -> Self {
+        Self {
+            github_owner: env::var("GITHUB_OWNER").unwrap(),
+            github_repo: env::var("GITHUB_REPO").unwrap(),
+            github_estimate_color_code: env::var("GITHUB_ESTIMATE_COLOR_CODE").unwrap()
+        }
+    }
 }
 
 #[tokio::main]
@@ -46,7 +63,7 @@ async fn main() -> Result<(), Error> {
                 println!("milestone title: {}", issues[0].milestone.title);
                 println!("number,title,estimate,state");
                 for issue in issues {
-                  let estimate_label = issue.labels.iter().find(|label| label.color == "C2E0C6").unwrap();
+                  let estimate_label = issue.labels.iter().find(|label| label.color == Config::new().github_estimate_color_code).unwrap();
                   println!("{},{},{},{}", issue.number, issue.title, estimate_label.name, issue.state)
                 };
             }
@@ -61,8 +78,8 @@ async fn main() -> Result<(), Error> {
 
 async fn fetch_issues(milestone_number: String) -> Result<Vec<Issue>, Error> {
     let request_url = format!("https://api.github.com/repos/{owner}/{repo}/issues",
-                              owner = "TomoroKobori",
-                              repo = "bissues");
+                              owner = Config::new().github_owner,
+                              repo = Config::new().github_repo);
     println!("{}", request_url);
     let client = reqwest::Client::new();
     let response = client.get(&request_url)
