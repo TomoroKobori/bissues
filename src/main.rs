5c4bbdd98a3405e505efa206b7811d6501122720
kobori <46224993+TomoroKobori@users.noreply.github.com>
2021-12-05 10:57:45 +0900

    feat: マイルストーンを指定してissuesを取得 このあとやること - リポジトリ名とユーザ名、カラーコードを環境変数から取得 - プライベートリポジトリからissuesを取得

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,23 @@ use serde::Deserialize;
 use reqwest::{Error};
 
 #[derive(Deserialize, Debug)]
-struct User {
-    login: String,
-    id: u32
+struct Issue {
+    number: i32,
+    title: String,
+    state: String,
+    labels: Vec<Label>,
+    milestone: Milestone
+}
+
+#[derive(Deserialize, Debug)]
+struct Label {
+    name: String,
+    color: String
+}
+
+#[derive(Deserialize, Debug)]
+struct Milestone {
+    title: String
 }
 
 #[tokio::main]
@@ -19,19 +33,25 @@ async fn main() -> Result<(), Error> {
             .about("select actions for [ls, vel]")
             .index(1)
             .required(true))
-        .arg(Arg::new("milestone")
-            .value_name("MILESTONE")
-            .about("select github milestones")
+        .arg(Arg::new("milestone_number")
+            .value_name("MILESTONE_NUMBER")
+            .about("select github milestone_number")
             .index(2)
             .required(true))
         .get_matches();
-    if let Some(milestone) = matches.value_of("milestone") {
+    if let Some(milestone_number) = matches.value_of("milestone_number") {
         if let Some(action) = matches.value_of("action") {
             if action == "ls" {
-                ls_issues(String::from(milestone)).await?;
+                let issues = fetch_issues(String::from(milestone_number)).await?;
+                println!("milestone title: {}", issues[0].milestone.title);
+                println!("number,title,estimate,state");
+                for issue in issues {
+                  let estimate_label = issue.labels.iter().find(|label| label.color == "C5DEF5").unwrap();
+                  println!("{},{},{},{}", issue.number, issue.title, estimate_label.name, issue.state)
+                };
             }
             if action == "vel" {
-                let velocity = fetch_velocity(String::from(milestone));
+                let velocity = fetch_velocity(String::from(milestone_number));
                 println!("velocity resurt: {}", velocity)
             }
         }
@@ -39,20 +59,19 @@ async fn main() -> Result<(), Error> {
     Ok(())
 }
 
-async fn ls_issues(milestone: String) -> Result<(), Error> {
-    let request_url = format!("https://api.github.com/repos/{owner}/{repo}/stargazers",
-                              owner = "rust-lang-nursery",
-                              repo = "rust-cookbook");
+async fn fetch_issues(milestone_number: String) -> Result<Vec<Issue>, Error> {
+    let request_url = format!("https://api.github.com/repos/{owner}/{repo}/issues",
+                              owner = "TomoroKobori",
+                              repo = "bissues");
     println!("{}", request_url);
     let client = reqwest::Client::new();
     let response = client.get(&request_url)
         .header("User-Agent", "request")
+        .query(&[("state", "all"), ("milestone", &milestone_number)])
         .send()
         .await?;
-    let users: Vec<User> = response.json().await?;
-    println!("{:?}", users);
-    println!("{}", milestone);
-    Ok(())
+    let issues: Vec<Issue> = response.json().await?;
+    Ok(issues)
 }
 
 fn fetch_velocity(velocity: String) -> String {
